3702cdc31550f5616260bf3b3f28e1bc1831ccd3
Gladox114 <53704522+Gladox114@users.noreply.github.com>
2019-11-02 03:23:33 +0100

    Added

---
+++ projects/Network/browser.lua
@@ -0,0 +1,54 @@
+function start()
+component = require("component")
+term = require("term")
+modem = component.modem
+event = require("event")
+ip = 8887
+--[[
+local arg = {...}
+if not arg[1] then
+    print("Connent to which ip?")
+    ip = io.read("*n")
+else ip = tonumber(arg[1]) end
+]]
+m = modem.broadcast
+
+modem.open(ip)
+term.clear()
+print([[
+(Space) Open door (temp)
+(J) Open door
+(K) Close door
+]])
+
+e = {event.pull(1,"key_down")}
+
+while true do
+    while true do
+        local e = {event.pull('key_down')}
+        
+        if e[4] == 57 then m(ip, 0x12) break
+        elseif e[4] == 36 then m(ip, 0x10) break
+        elseif e[4] == 37 then m(ip, 0x11) break
+        elseif e[3] == 15 and e[4] == 24 then os.exit()
+        end
+    end
+
+    local _, _, from, port, _, message = event.pull(10,"modem_message")
+    if not message then 
+        print("No Response") 
+    else 
+        if message == 1 then
+            print("Open")
+        elseif message == 0 then
+           print("Closed") 
+        elseif message == 2 then
+            print("Opening Temporarly")
+        end
+        --print("Shutting down...")
+        --os.shutdown() 
+        os.sleep(0.1)
+    end
+    
+end
+end
---
+++ projects/Network/server.lua
@@ -0,0 +1,79 @@
+--[[
+component = require("component")
+modem = require("modem")
+rs = component.redstone
+event = require("event")
+ip = 8887
+side = 4
+
+modem.open(ip)
+
+--while true do
+local _,_, From, port, _, message = event.pull("modem_message")
+print(message)
+x = message:find " "
+command = message:sub(0,x)
+clock = message:sub(x,-0)
+
+if clock > os.clock() and os.clock()+0.1 > clock then
+    if command = 0x10 then
+        rs.setOuput(side,15)
+        modem.send(from, ip, 1)
+    elseif command 0x20 then
+        rs.setOutput(side, 0)
+        modem.send(from, ip, 0)
+    elseif command 0x30 then
+        local Input = rs.getInput()
+        modem.send(from, ip, Input)
+    end
+end
+
+--end
+]]
+
+
+component = require("component")
+modem = component.modem
+rs = component.redstone
+event = require("event")
+term = require("term")
+
+ip = 8887
+side = 5
+closedown = 5
+
+modem.open(ip)
+term.clear()
+
+function close()
+    rs.setOutput(side, 0)
+end
+
+while true do
+    while true do
+        local _,_, from, port, _, message = event.pull(1,"modem_message")
+        if message then break end
+    end
+print(from, message)
+command = message
+
+if command == 0x10 then
+    rs.setOutput(side, 15)
+    print(from.." OK")
+    modem.send(from, ip, 1)
+elseif command == 0x11 then
+    rs.setOutput(side, 0)
+    print(from.." OK")
+    modem.send(from, ip, 0)
+elseif command == 0x12 then
+    rs.setOutput(side, 15)
+    print(from.." OK")
+    modem.send(from, ip, 2)
+    timex = event.timer(closedown, close, 1)
+--elseif command 0x30 then
+--    local Input = rs.getInput()
+--    modem.send(from, ip, Input)
+end
+
+os.sleep(0.1)
+end
