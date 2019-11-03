c2326ce33b6538e32ad02acdddce38e2af6251b9
Gladox114 <53704522+Gladox114@users.noreply.github.com>
2019-11-04 00:18:34 +0100

    Added better button that works

--- projects/Network/V3/App.lua
+++ projects/Network/V3/App.lua
@@ -1,8 +1,10 @@
 local GUI = require("GUI")
+local buffer = require("doubleBuffering")
 local term = require("term")
 local c = require("component")
 local m = c.modem
-local Tinput = ""
+local color1,color2 = 0x2D2D2D,0xC3C3C3--0xC3C3C3,0xA5A5A5
+local IP = 8887
 --------------------------------------------------------------------------------
 
 local label_top = "Enrichment Center Control Tablet"
@@ -25,33 +27,68 @@ application.eventHandler = function(application,object,e1,e2,e3,e4)
         end
     end
 end
+--[[
+function settings()
 
+    local application2 = GUI.application(application.width/2-20,application.height/2-4,40,8)
 
+    local oldPixels = buffer.copy(application2.x, application2.y, application2.width, application2.height)
 
+    local textfield = application2:addChild(GUI.input(2,2,30,3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "", "Input IP", false))
+
+    application2:addChild(GUI.button(1,3,36,3,0xA5A5A5,0xFFFFFF, 0x696969, 0xFFFFFF,"OK")).onTouch = function()
+        IP = textfield.text
+        application2:stop()
+        buffer.paste(application2.x, application2.y, oldPixels)
+		buffer.drawChanges()
+    end 
+end
+]]
 
 -- Add a panel
-application:addChild(GUI.panel(1, 1, application.width, application.height, 0xC3C3C3))
-application:addChild(GUI.panel(application.width/2-label_top:len()/2-1,2, label_top:len()+2, 3, 0xA5A5A5))
+application:addChild(GUI.panel(1, 1, application.width, application.height, color1))
+application:addChild(GUI.panel(application.width/2-label_top:len()/2-1,2, label_top:len()+2, 3, color2))
 
 -- Add a Label
 application:addChild(GUI.text( application.width/2-label_top:len()/2 , 3 , 0xFF0000, label_top ))
 
 -- Add a button to application and .onTouch() method
-application:addChild(GUI.button(1, 1, 10, 3, 0xE1E1E1, 0x4B4B4B, 0xA5A5A5, 0x0, "Settings")).onTouch = function()
-	-- Add a background container to application with background panel and layout
-	local container = GUI.addBackgroundContainer(application, true, true, "Settings")
+application:addChild(GUI.button(1, 1, 10, 3, 0xE1E1E1, 0x4B4B4B, color2, 0x0, "Settings")).onTouch = function()
+
+    
+
+
+
+    local container = application:addChild(GUI.container(1, 1, application.width, application.height))
+
+    container.panel = container:addChild(GUI.panel(1, 1, container.width, container.height, GUI.BACKGROUND_CONTAINER_PANEL_COLOR, GUI.BACKGROUND_CONTAINER_PANEL_TRANSPARENCY))
+    container.panel.eventHandler = function(application, object, e1)
+        if e1 == "touch" then
+            container:remove()
+            application:draw()
+        end
+    end
+
+    container.layout = container:addChild(GUI.layout(1, 1, container.width, container.height, 1, 1))
+    container.label = container.layout:addChild(GUI.label(1, 1, 1, 1, GUI.BACKGROUND_CONTAINER_TITLE_COLOR, "Settings")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
+    -- Add a background container to application with background panel and layout
+    --local container = GUI.addBackgroundContainer(application, true, true, "Settings")
     -- Add a switch and label to it's layout
     
     --container.layout:addChild(GUI.switchAndLabel(1, 1, 36, 8, 0x66DB80, 0x2D2D2D, 0xE1E1E1, 0x878787, "I like to suck big dicks:", true))
 
-    local textfield = container.layout:addChild(GUI.input(1, 1, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, Tinput, "Input IP"))
-    textfield
-    textfield.validator = function(Tinput)
-        GUI.alert("Input:"..Tinput)
-        textfield.close
+    local textfield = container.layout:addChild(GUI.input(1, 1, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, newIP or "" , "Input IP", false))
+    
+    container.layout:addChild(GUI.button(1,3,10,3,0xA5A5A5,0xFFFFFF, 0x696969, 0xFFFFFF,"OK")).onTouch = function()
+        newIP = textfield.text
+        container:remove()
+        application:draw()
     end
+--    textfield.validator = function(Tinput)
+--        GUI.alert("Input:"..Tinput)
+--        textfield.close
+--    end
 end
-
 
 
 
