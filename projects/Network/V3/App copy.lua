61a17d6a610401c39bee5de028898013e2d92310
Gladox114 <53704522+Gladox114@users.noreply.github.com>
2019-11-06 19:38:40 +0100

    Added FileSystem and better close application

--- projects/Network/V3/App copy.lua
+++ projects/Network/V3/App copy.lua
@@ -1,7 +1,3 @@
--- # Open source GUI made by Gladox114
--- # github.com/Gladox114
--- # https://github.com/Gladox114/OpenComputers-Project/tree/master/projects/Network/V3
-
 local GUI = require("GUI")
 local buffer = require("doubleBuffering")
 local term = require("term")
@@ -9,7 +5,7 @@ local c = require("component")
 local m = c.modem
 local color1,color2 = 0x2D2D2D,0xC3C3C3--0xC3C3C3,0xA5A5A5
 local ip = 8887
-
+-- not working...
 local function confRead()
 local file = io.open("config.txt","r")
 if file then
@@ -31,7 +27,7 @@ application.eventHandler = function(application,object,e1,e2,e3,e4)
         if e3 == 15 and e4 == 24 then
             application:addChild(GUI.panel(1, 1, application.width, application.height, 0x000000))
             application:draw()
-            application:stop() -- # new method i didn't saw
+            os.exit()
         elseif e4 == 57 then 
             m(ip, 0x12)
         elseif e4 == 36 then
@@ -41,6 +37,23 @@ application.eventHandler = function(application,object,e1,e2,e3,e4)
         end
     end
 end
+--[[
+function settings()
+
+    local application2 = GUI.application(application.width/2-20,application.height/2-4,40,8)
+
+    local oldPixels = buffer.copy(application2.x, application2.y, application2.width, application2.height)
+
+    local textfield = application2:addChild(GUI.input(2,2,30,3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "", "Input IP", false))
+
+    application2:addChild(GUI.button(1,3,36,3,0xA5A5A5,0xFFFFFF, 0x696969, 0xFFFFFF,"OK")).onTouch = function()
+        IP = textfield.text
+        application2:stop()
+        buffer.paste(application2.x, application2.y, oldPixels)
+		buffer.drawChanges()
+    end 
+end
+]]
 
 -- Add a panel
 application:addChild(GUI.panel(1, 1, application.width, application.height, color1))
@@ -49,9 +62,11 @@ application:addChild(GUI.panel(application.width/2-label_top:len()/2-1,2, label_
 -- Add a Label
 application:addChild(GUI.text( application.width/2-label_top:len()/2 , 3 , 0xFF0000, label_top ))
 
------- Settings -----
 -- Add a button to application and .onTouch() method
 application:addChild(GUI.button(1, 1, 10, 3, 0xE1E1E1, 0x4B4B4B, color2, 0x0, "Settings")).onTouch = function()
+
+    
+
 
     -- Add a container to application
     local container = application:addChild(GUI.container(1, 1, application.width, application.height))
@@ -67,7 +82,14 @@ application:addChild(GUI.button(1, 1, 10, 3, 0xE1E1E1, 0x4B4B4B, color2, 0x0, "S
     container.layout = container:addChild(GUI.layout(1, 1, container.width, container.height, 1, 1))
     -- Add a label to layout
     container.label = container.layout:addChild(GUI.label(1, 1, 1, 1, GUI.BACKGROUND_CONTAINER_TITLE_COLOR, "Settings")):setAlignment(GUI.ALIGNMENT_HORIZONTAL_CENTER, GUI.ALIGNMENT_VERTICAL_TOP)
-    
+
+--[[
+    -- Add a background container to application with background panel and layout
+    --local container = GUI.addBackgroundContainer(application, true, true, "Settings")-- #not usefull, no option to remove object after pressing button. I copied from the GUI.lua the function and edited it above so you can see how that function was made.
+    -- Add a switch and label to it's layout
+    --container.layout:addChild(GUI.switchAndLabel(1, 1, 36, 8, 0x66DB80, 0x2D2D2D, 0xE1E1E1, 0x878787, "I like to suck big dicks:", true))-- #not usefull
+]]
+
     -- Add a input field to layout
     local textfield = container.layout:addChild(GUI.input(1, 1, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, newIP or IP or "" , "Input IP", false))
     -- Add a button to layout
@@ -82,6 +104,46 @@ application:addChild(GUI.button(1, 1, 10, 3, 0xE1E1E1, 0x4B4B4B, color2, 0x0, "S
     end
 end
 
+
+
+
+
+
+--[[
+-- Add a regular button
+local regularButton = application:addChild(GUI.button(2, 2, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Regular button"))
+regularButton.onTouch = function()
+	GUI.alert("Regular button was pressed")
+end
+
+-- Add a regular button with disabled state
+local disabledButton = application:addChild(GUI.button(2, 6, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Disabled button"))
+disabledButton.disabled = true
+
+-- Add a regular button with switchMode state
+local switchButton = application:addChild(GUI.button(2, 10, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Switch button"))
+switchButton.switchMode = true
+switchButton.onTouch = function()
+	GUI.alert("Switch button was pressed")
+end
+
+-- Add a regular button with disabled animation
+local notAnimatedButton = application:addChild(GUI.button(2, 14, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Not animated button"))
+notAnimatedButton.animated = false
+notAnimatedButton.onTouch = function()
+	GUI.alert("Not animated button was pressed")
+end
+
+-- Add a rounded button
+application:addChild(GUI.roundedButton(2, 18, 30, 3, 0xFFFFFF, 0x555555, 0x880000, 0xFFFFFF, "Rounded button")).onTouch = function()
+	GUI.alert("Rounded button was pressed")
+end
+
+-- Add a framed button
+application:addChild(GUI.framedButton(2, 22, 30, 3, 0xFFFFFF, 0xFFFFFF, 0x880000, 0x880000, "Framed button")).onTouch = function()
+	GUI.alert("Framed button was pressed")
+end
+]]
 --------------------------------------------------------------------------------
 
 application:draw(true)
